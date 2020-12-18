edbfd0df31c91d5e216ee2adc930b2156bf6414b
Egon Rijpkema <git@egonrijpkema.nl>
2020-12-18 13:57:33 +0100

    Cleaned up day 18.

--- day18.py
+++ day18.py
@@ -16,41 +16,28 @@ class Infix(object):
     def __or__(self, other):
         return self.function(other)
 
-    def __rlshift__(self, other):
-        return Infix(lambda x, self=self, other=other: self.function(other, x))
-
-    def __rshift__(self, other):
-        return self.function(other)
-
-    def __call__(self, value1, value2):
-        return self.function(value1, value2)
-
 
 mul = Infix(lambda x, y: x * y)
 add = Infix(lambda x, y: x + y)
 
 
-def process(line):
-    return eval(line.replace('*', '|mul|').replace('+', '|add|'))
-
-
-def process2(line):
-    return eval(line.replace('*', '|mul|'))
+def process(line, part=1):
+    line = line.replace('*', '|mul|')
+    if part == 1:
+        line = line.replace('+', '|add|')
+    return eval(line)
 
 
 if __name__ == '__main__':
     assert process('2 * 3 + (4 * 5)') == 26
 
     count = 0
+    count2 = 0
     with open('input18.txt') as f:
         for line in f:
-            count += process(line)
+            count += process(line, part=1)
+            count2 += process(line, part=2)
 
     print('Answer part 1: {}'.format(count))
 
-    count = 0
-    with open('input18.txt') as f:
-        for line in f:
-            count += process2(line)
-
-    print('Answer part 2: {}'.format(count))
+    print('Answer part 2: {}'.format(count2))
